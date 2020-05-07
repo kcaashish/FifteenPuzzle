63f573307e7bec4b11ae6b6992058ac7e6c2ee1f
kcaashish <kcaashish@gmail.com>
2020-05-07 12:14:43 +0545

    Initial commit

---
+++ FifteenPuzzle.java
@@ -0,0 +1,84 @@
+/**
+ * FifteenPuzzle maintains the internal representation of a 15 puzzle. 
+ * This is a 4x4 2D array of integers, numbered 0..15. 0 denotes the space. 
+ * A legal move is to shift the space to an adjacent square on the grid. 
+ * The puzzle is finished when all tiles are in the correct position as defined by goal. 
+ * 
+ * @author  Aashish K.C.
+ * @version May 2020
+ */
+
+public class FifteenPuzzle
+{ 
+    private       int[][] grid; // the current positions of the tiles and the space, denoted by 0..15
+    private       int xSpace;   // xspace,yspace are the current coordinates of the space
+    private       int ySpace;
+    private final int size = 4; // the number of tiles across and down 
+    private final int[][] goal = {{1,5,9,13}, {2,6,10,14}, {3,7,11,15}, {4,8,12,0}}; // the tile positions in the goal state
+    
+    // these two are public and static so that they can be used in BlueJ 
+    // when creating an object, enter e.g. FifteenPuzzle.close4 in the argument box
+    public static final int[][] example4 = {{ 5,11,14, 0}, { 9, 3,13, 7}, { 2, 8,10,12}, { 4, 1,15, 6}};
+    public static final int[][] close4   = {{ 1, 5, 9,13}, { 2, 6,10,14}, { 3, 7,11, 0}, { 4, 8,12,15}};
+    
+    /** This constructor sets up the grid using initialGrid, and it initialises xSpace and ySpace.
+     *  initialGrid is assumed to be a legal position.
+     */
+    public FifteenPuzzle (int[][] initialGrid)
+    {
+        // TODO 1
+    }
+    
+    /** This constructor sets up the grid by copying goal and then making random moves. 
+     *  It also initialises xSpace and ySpace.
+     */
+    public FifteenPuzzle ()
+    {
+        // TODO 8
+    }
+    
+    /**
+     * Returns the grid.
+     */
+    public int[][] getGrid()
+    {
+        return grid;
+    }
+       
+    /**
+     * Returns the size.
+     */ 
+    public int getSize()
+    {
+        return size;
+    }
+       
+    /**
+     * Returns true iff x,y is on the board and adjacent to the space. 
+     * Either x must be xspace and y must be yspace plus or minus 1, 
+     * or     y must be yspace and x must be xspace plus or minus 1.
+     */
+    public boolean legalClick(int x, int y)
+    {
+        // TODO 5
+        return true;
+    }
+    
+    /**
+     * Returns true iff the puzzle is in the goal state.
+     */
+    public boolean finished()
+    {
+        // TODO 7
+        return false;
+    }
+    
+    /**
+     * If x,y is a legal click on the board, swaps the tile at x,y with the space;
+     * otherwise does nothing.
+     */ 
+    public void moveTile (int x, int y) 
+    {
+        // TODO 6
+    }
+}
---
+++ FifteenPuzzleViewer.java
@@ -0,0 +1,40 @@
+/**
+ * FifteenPuzzleViewer displays a 15 puzzle and interacts with the user.
+ * 
+ * @author Aashish K.C.
+ * @version May 2020
+ */
+
+import java.awt.event.*;
+import java.awt.*;
+
+public class FifteenPuzzleViewer 
+{
+    private FifteenPuzzle puzzle; // the internal puzzle 
+    private int           size;   // the size of the puzzle
+    private SimpleCanvas  sc;     // the canvas for display 
+    
+    /**
+     * Sets up the three fields and draws the initial puzzle.
+     */
+    public FifteenPuzzleViewer(FifteenPuzzle puzzle) 
+    {
+        // TODO 4
+    }
+    
+    /**
+     * Draws all of the tiles on sc.
+     */
+    private void drawGrid()
+    {
+        // TODO 3
+    }
+    
+    /**
+     * Draws the tile at x,y in colour c at the appropriate spot on sc.
+     */
+    private void drawTile(int x, int y, Color c)
+    {
+        // TODO 2
+    }
+}
